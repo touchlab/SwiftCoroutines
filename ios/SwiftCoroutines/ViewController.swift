57cb201931d2417dd51a952e2627e417529aaa0d
russell <russell@touchlab.co>
2021-04-13 17:18:47 -0400

    Add combine example in-app

--- ios/SwiftCoroutines/ViewController.swift
+++ ios/SwiftCoroutines/ViewController.swift
@@ -9,26 +9,44 @@
 import UIKit
 import shared
 import RxSwift
+import Combine
 
 class ViewController: UIViewController {
 
     let repository = ThingRepositoryIos(repository: ThingRepository())
     var disposable: Disposable? = nil
+    var cancellable: AnyCancellable? = nil
 
     override func viewDidAppear(_ animated: Bool) {
         disposable = createObservable(flowWrapper: repository.getThingStreamWrapper(count: 3, succeed: true))
             .subscribe(
                 onNext: { thing in
-                    NSLog("next: \(thing)")
+                    NSLog("RxSwift next: \(thing)")
                 },
                 onError: { (error: Error) in
-                    NSLog("error: \(error.localizedDescription)")
+                    NSLog("RxSwift error: \(error.localizedDescription)")
                 },
                 onCompleted: {
-                    NSLog("complete!")
+                    NSLog("RxSwift complete!")
                 },
                 onDisposed: {
-                    NSLog("disposed!")
+                    NSLog("RxSwift disposed!")
+                }
+            )
+        
+        cancellable = createPublisher(flowWrapper: repository.getThingStreamWrapper(count: 3, succeed: true))
+            .handleEvents(receiveCancel: { NSLog("Combine canceled!") })
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        NSLog("Combine error: \(error.localizedDescription)")
+                    case .finished:
+                        NSLog("Combine finished!")
+                    }
+                },
+                receiveValue: { thing in
+                    NSLog("Combine next: \(thing)")
                 }
             )
     }
@@ -36,6 +54,9 @@ class ViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         disposable?.dispose()
         disposable = nil
+        
+        cancellable?.cancel()
+        cancellable = nil
     }
 }
 
